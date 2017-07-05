61e80b96aa4b5c9fa9bd7fbbfc5e8ab18b256056
王 <122589615@qq.com>
2017-07-05 13:33:40 +0800

    Initial commit

---
+++ LLTimerTableView/LLTimerTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LLTimerTableViewCell.h
+//  LLTimerTableView
+//
+//  Created by WangZhaomeng on 2017/7/5.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLTimerTableViewCell : UITableViewCell
+
+@end
---
+++ LLTimerTableView/LLTimerTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  LLTimerTableViewCell.m
+//  LLTimerTableView
+//
+//  Created by WangZhaomeng on 2017/7/5.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import "LLTimerTableViewCell.h"
+
+@implementation LLTimerTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
